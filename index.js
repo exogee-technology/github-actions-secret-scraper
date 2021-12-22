f36bb0d3ea9a1f8072fcb8dfecf5bb969e8456cf
Kevin Brown <kevin.brown@exogee.com>
2021-12-22 19:17:29 +1100

    Skip parameter if it's not a string or is empty.

--- index.js
+++ index.js
@@ -10,7 +10,9 @@ const run_action = async () => {
 
 		for (const [variable, value] of Object.entries(process.env)) {
 			if (variable.startsWith('AWS_')) {
-				core.debug(`Skipping paramter ${variable}`);
+				core.debug(`Skipping AWS paramter ${variable}`);
+			} else if (typeof value !== 'string' || value.length === 0) {
+				core.debug(`Skipping empty / non-string variable ${variable}`);
 			} else {
 				core.debug(`Putting parameter ${variable}`);
 
